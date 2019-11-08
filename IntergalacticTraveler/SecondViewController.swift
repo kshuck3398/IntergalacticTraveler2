7e1cd9fd05972d6742701685eda3de53fdc5aa7e
Kellen Shuck <kshuck3398@stu.d214.org>
2019-11-08 11:27:30 -0600

    Added second view controller file to second view controller and connected image view to second view controller

---
+++ IntergalacticTraveler/SecondViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SecondViewController.swift
+//  IntergalacticTraveler
+//
+//  Created by Kellen Shuck on 11/8/19.
+//  Copyright © 2019 Kekeke21. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    @IBOutlet weak var starImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
